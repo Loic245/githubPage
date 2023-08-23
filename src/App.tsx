3d9e236fb4add65b4a2fa6fa9df8bef7fef91ecb
Loic245 <bradjack24ricks@gmail.com>
2023-08-23 18:44:28 +0300

    delete browserRouter and show only component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import { BrowserRouter } from "react-router-dom";
 import Router from "./Route/Route";
 import "./App.css";
+import Accueil from "./components/Accueil";
+import Experience from "./components/experience";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Router />
-    </BrowserRouter>
+    // <BrowserRouter>
+    //   <Router />
+    // </BrowserRouter>
+    <div>
+      <Accueil />
+      <Experience />
+    </div>
   );
 }
 
